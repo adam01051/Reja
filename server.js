c10d32425b57075650d8d20b1c7ee2ccc8bb1ca3
adam01051 <lloki0105@gmail.com>
2025-12-26 06:11:50 +0500

    feat: finished publishing

--- server.js
+++ server.js
@@ -1,28 +1,39 @@
 const express = require("express");
 const app = express();
 const http = require("http");
+const fs = require("fs");
 
-//1 
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+	if (err) {
+		console.log("ERROR: ", err);
+	} else {
+		user = JSON.parse(data);
+	}
+});
+//1
 app.use(express.static("public"));
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
 //2 session
 
-
-//3 BSSR: 
+//3 BSSR:
 app.set("views", "views");
 app.set("view engine", "ejs");
-
 
 //4
 
 app.post("/create-item", function (req, res) {
-	//todo: code with db here
+	// res.end("  <h1>hello world</h1>");
+	//to do it later
+});
+app.get("/author", function (req, res) {
+	res.render("author", { user: user });
 });
 
 app.get("/", function (req, res) {
-	res.render("harid");
+	res.render("harid.ejs");
 });
 
 
